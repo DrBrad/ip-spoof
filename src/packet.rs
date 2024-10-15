3ebc91f188fe0f5831ab46d863953c436203ca2f
DrBrad <brad@bradeagle.com>
2024-10-15 09:33:45 -0600

    TCP support

--- src/packet.rs
+++ src/packet.rs
@@ -159,44 +159,6 @@ impl TcpPacket {
         }
     }
 
-    pub fn encode(&self) -> Vec<u8> {
-        let total_length = 20 + 8 + self.payload.len(); // IP header (20 bytes) + UDP header (8 bytes)
-
-        let ip_header = IpHeader {
-            version_ihl: (4 << 4) | 5, // IPv4, header length 5 (20 bytes)
-            tos: 0,
-            length: (total_length as u16).to_be(),
-            id: 0,
-            flags_offset: 0,
-            ttl,
-            protocol: libc::IPPROTO_UDP as u8,
-            checksum: 0,
-            src_ip: u32::from(self.src_addr).to_be(),
-            dst_ip: u32::from(self.dst_addr).to_be(),
-        };
-
-        let udp_length = (8 + self.payload.len()) as u16;
-        let udp_header = UdpHeader {
-            src_port: self.src_port.to_be(),
-            dst_port: self.dst_port.to_be(),
-            length: udp_length.to_be(),
-            checksum: 0
-        };
-
-        let mut packet = Vec::with_capacity(total_length);
-        let ip_header_bytes: [u8; 20] = unsafe { mem::transmute(ip_header) };
-        let udp_header_bytes: [u8; 8] = unsafe { mem::transmute(udp_header) };
-
-        packet.extend_from_slice(&ip_header_bytes);
-        packet.extend_from_slice(&udp_header_bytes);
-        packet.extend_from_slice(self.payload.as_slice());
-
-        let ip_checksum = self.checksum(&packet[..20]);
-        packet[10..12].copy_from_slice(&ip_checksum.to_be_bytes());
-
-        packet
-    }
-
     fn checksum(&self, data: &[u8]) -> u16 {
         let mut sum = 0u32;
         let len = data.len();
