26e44ce7f00b81fdbed74845a59931b312aa0c9b
DrBrad <brad@bradeagle.com>
2024-10-15 09:31:41 -0600

    TCP support

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,6 @@ mod tests {
     #[test]
     fn test() {
         let socket = RawSocket::new(Ipv4Addr::new(8, 1, 8, 1), 4333);
-        socket.send_udp_packet(Ipv4Addr::new(127, 0, 0, 1), 8080, b"hello world").expect("Failed to send packet");
+        socket.send_udp_packet(Ipv4Addr::new(127, 0, 0, 1), 8080, b"hello world", 64).expect("Failed to send packet");
     }
 }
